8f99f48d763dd6ae2472e5f748afe99afaa6bcf7
= <noderoid64@gmail.com>
2018-05-29 22:45:39 +0300

    (feature) add log classes

--- FightClub/FightClub/FightClub/Tools/Loger.cs
+++ FightClub/FightClub/FightClub/Tools/Loger.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace FightClub.Tools
+namespace FightClub
 {
-    static class Loger
+    abstract class Loger
     {
+       protected Loger loger;
+
+        public Loger(Loger log)
+        {
+            loger = log;
+        }
+       public abstract void Write(string message);
+    }
+
+    class FileLoger : Loger
+    {
+        public FileLoger(Loger log) : base(log) { }
+
+        public override void Write(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class UILoger : Loger
+    {
+        IMainView View;
+        public UILoger(IMainView view, Loger log = null) : base(log) { }
+
+        public override void Write(string message)
+        {
+            View.Log(message);
+            if (loger != null)
+                loger.Write(message);
+        }
+    }
+    class DebugLoger : Loger
+    {
+
+        public DebugLoger(Loger loger = null) : base(loger) { }
+        
+        public override void Write(string message)
+        {
+            Debug.WriteLine(message);
+            if (loger != null)
+                loger.Write(message);
+        }
     }
 }
